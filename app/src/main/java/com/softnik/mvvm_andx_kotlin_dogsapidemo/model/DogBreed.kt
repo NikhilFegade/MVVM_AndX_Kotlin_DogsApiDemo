75f11b758f99f803e52b796ceb2fae5f01e670d3
Nikhil Fegade <nikhilfegade17@gmail.com>
2020-07-13 19:22:03 +0530

    Added Retrofit

--- app/src/main/java/com/softnik/mvvm_andx_kotlin_dogsapidemo/model/DogBreed.kt
+++ app/src/main/java/com/softnik/mvvm_andx_kotlin_dogsapidemo/model/DogBreed.kt
@@ -1,12 +1,27 @@
 package com.softnik.mvvm_andx_kotlin_dogsapidemo.model
 
+import com.google.gson.annotations.SerializedName
+
 data class DogBreed(
+    @SerializedName("id")
     val breedId: String?,
+
+    @SerializedName("names")
     val dogBreed: String?,
+
+    @SerializedName("life_span")
     val lifeSpan: String?,
+
+    @SerializedName("breed_group")
     val breedGroup: String?,
+
+    @SerializedName("bred_for")
     val breedFor: String?,
+
+    @SerializedName("temperament")
     val temperament: String?,
+
+    @SerializedName("url")
     val imageUrl: String?
 
 )
---
+++ app/src/main/java/com/softnik/mvvm_andx_kotlin_dogsapidemo/model/DogsApi.kt
@@ -0,0 +1,9 @@
+package com.softnik.mvvm_andx_kotlin_dogsapidemo.model
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface DogsApi {
+    @GET("DevTides/DogsApi/master/dogs.json")
+    fun getDogs():Single<List<DogBreed>>
+}
